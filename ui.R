c648bf573c87fd96350cff0e109c8a155f1e709c
chisg <chihyun.song101@gmail.com>
2018-03-07 01:19:48 -0800

    widgets should be in order

--- ui.R
+++ ui.R
@@ -18,34 +18,27 @@ my.ui <- fluidPage(
   titlePanel <- h1("Factors Correlated to Violent Crimes in Seattle"),
   theme = shinytheme("darkly"),
   
-  sidebarLayout(
-    mainPanel(
-      tabsetPanel(type="tabs",
-                  tabPanel("Introduction", includeMarkdown("introduction.md")),
-                  tabPanel("Map",leafletOutput("mymap")),
-                  tabPanel("Correlated Factors", plotOutput("distPlot")),
-                  tabPanel("Regression Table", imageOutput("table"),  textOutput("description"))
-      )
-      ),
-    sidebarPanel(
-      sliderInput("sliderTime", label = h3("Time (Year)"), min = 2012, 
-                  max = 2018, value = c(2014, 2016), round = -1, step = 0.5,
-                  sep = ""
-      ),
-      selectInput("selectCrime", h3("Type of Crime"),
-                  c("Burglary" = "bur",
-                    "Robbery" = "rob",
-                    "Assault" = "ass",
-                    "Vehicle Theft" = "vt",
-                    "Threats" = "thr",
-                    "Homicide" = "hom")
-      ),
-      selectInput("select", label = h3("Select Factor"), 
-                  choices = list("Unemployment Rate" = 1, "Average Rent" = 2, "Cocaine 
-                                 Arrests" = 3, "Consumer Index Price of Food" = 4), 
-                  selected = 1),
-      
-      textOutput("variables")
+  mainPanel(
+    tabsetPanel(type="tabs",
+      tabPanel("Introduction", includeMarkdown("introduction.md")),
+      tabPanel("Map",leafletOutput("mymap"), sliderInput("sliderTime", label = h3("Time (Year)"), min = 2012, 
+                                                         max = 2018, value = c(2014, 2016), round = -1, step = 0.5,
+                                                         sep = ""),
+               selectInput("selectCrime", h3("Type of Crime"),
+                           c("Burglary" = "bur",
+                             "Robbery" = "rob",
+                             "Assault" = "ass",
+                             "Vehicle Theft" = "vt",
+                             "Threats" = "thr",
+                             "Homicide" = "hom"))
+             ),
+      tabPanel("Correlated Factors", plotOutput("distPlot"), selectInput("select", label = h3("Select Factor"), 
+                                                                        choices = list("Unemployment Rate" = 1, "Average Rent" = 2, "Cocaine 
+                                                                        Arrests" = 3, "Consumer Index Price of Food" = 4), 
+                                                                        selected = 1),
+                           
+        textOutput("variables")),
+      tabPanel("Regression Table", imageOutput("table"),  textOutput("description"))
     )
   )
 )
