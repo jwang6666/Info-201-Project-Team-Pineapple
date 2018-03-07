9e8cbb12d0230c4c27d1039a9adc02f66a307d52
jwang6666 <jwang26@uw.edu>
2018-03-07 11:26:16 -0800

    change to navbarPage

--- ui.R
+++ ui.R
@@ -14,33 +14,56 @@ library("scales")
 library("markdown")
 library("shinythemes")
 
-my.ui <- fluidPage(
-  titlePanel <- h1("Factors Correlated to Violent Crimes in Seattle"),
+my.ui <- navbarPage(
+  "Factors Correlated to Violent Crimes in Seattle",
   theme = shinytheme("darkly"),
   
-  mainPanel(
-    tabsetPanel(type="tabs",
-      tabPanel("Introduction", includeMarkdown("introduction.md")),
-      tabPanel("Map",leafletOutput("mymap"), sliderInput("sliderTime", label = h3("Time (Year)"), min = 2012, 
-                                                         max = 2018, value = c(2014, 2016), round = -1, step = 0.5,
-                                                         sep = ""),
-               selectInput("selectCrime", h3("Type of Crime"),
-                           c("Burglary" = "bur",
-                             "Robbery" = "rob",
-                             "Assault" = "ass",
-                             "Vehicle Theft" = "vt",
-                             "Threats" = "thr",
-                             "Homicide" = "hom"))
+  tabPanel("Introduction",
+             includeMarkdown("introduction.md")
+  ),
+  
+  tabPanel("Map",
+      
+      sidebarPanel(
+             sliderInput("sliderTime", label = h3("Time (Year)"), min = 2012, 
+                         max = 2018, value = c(2014, 2016), round = -1, step = 0.5,
+                         sep = ""
              ),
-      tabPanel("Correlated Factors", plotOutput("distPlot"), selectInput("select", label = h3("Select Factor"), 
-                                                                        choices = list("Unemployment Rate" = 1, "Average Rent" = 2, "Cocaine 
-                                                                        Arrests" = 3, "Consumer Index Price of Food" = 4), 
-                                                                        selected = 1),
-                           
-        textOutput("variables")),
-      tabPanel("Regression Table", imageOutput("table"),  textOutput("description"))
-    )
+             selectInput("selectCrime", h3("Type of Crime"),
+                         c("Burglary" = "bur",
+                           "Robbery" = "rob",
+                           "Assault" = "ass",
+                           "Vehicle Theft" = "vt",
+                           "Threats" = "thr",
+                           "Homicide" = "hom")
+             )
+           ),
+      mainPanel(
+        leafletOutput("mymap")
+      )
+      
+  ),
+  
+  tabPanel("Correlated Factors",
+           sidebarPanel(
+             selectInput("select", label = h3("Select Factor"), 
+                         choices = list("Unemployment Rate" = 1, "Average Rent" = 2, "Cocaine 
+                                        Arrests" = 3, "Consumer Index Price of Food" = 4), 
+                         selected = 1),
+             
+             textOutput("variables")
+           ),
+           
+           mainPanel(plotOutput("distPlot"))
+  ),
+  
+  tabPanel("Regression Table",
+
+             imageOutput("table"),  
+             textOutput("description")
+           
   )
+
 )
 
 shinyUI(my.ui)
